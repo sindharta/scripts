bd4046492b4365c7a82c49c229999d3095c6affb
Sindharta Tanuwijaya <>
2026-03-29 22:18:18 +0900

    separate into modules

---
+++ .hammerspoon/modules/screenshot-to-preview.lua
@@ -0,0 +1,31 @@
+-- ScreenShot to Clipboard
+hs.hotkey.bind({"cmd", "alt"}, "P", function()
+    hs.eventtap.keyStroke({"cmd", "shift", "ctrl"}, "4")
+
+    local before = hs.pasteboard.changeCount()
+    local startTime = hs.timer.secondsSinceEpoch()
+
+    hs.timer.doEvery(0.2, function(timer)
+        local now = hs.timer.secondsSinceEpoch()
+        -- give user time to select area
+        if now - startTime < 1 then
+            return
+        end
+
+        if hs.pasteboard.changeCount() ~= before then
+            local preview = hs.appfinder.appFromName("Preview")
+            if preview then
+                preview:selectMenuItem({"File", "New from Clipboard"})
+            end
+            timer:stop()            
+            return
+        end
+
+        if now - startTime > 10 then
+            timer:stop()
+            hs.alert.show("Screenshot timeout")
+        end
+    end)
+    
+end)
+
